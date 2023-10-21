059a25153cee63dd9a35235c70ca738be4c9deb7
Алексей Грабовский <leha.grabovskiy.00@bk.ru>
2023-10-22 01:59:49 +0300

    3rd Commit - home_task 20_2 - solved

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from catalog.views import home_page, contact_info
+from catalog.views import home_page, contact_info, banana, potato, apple, step, pork
 
 urlpatterns = [
     path('', home_page),
-    path('contacts', contact_info)
+    path('contacts', contact_info),
+    path('banana', banana),
+    path('potato', potato),
+    path('apple', apple),
+    path('step', step),
+    path('pork', pork)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,60 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home_page(request):
-    return render(request, 'catalog/home_page.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home_page.html', context)
 
 
 def contact_info(request):
-    return render(request, 'catalog/contact_info.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contact_info.html', context)
+
+
+
+def banana(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'catalog/banana.html', context)
+
+
+def potato(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'catalog/potato.html', context)
+
+
+def pork(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'catalog/pork.html', context)
+
+
+def apple(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'catalog/apple.html', context)
+
+
+def step(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'catalog/step.html', context)
